6436ffc0bdfab3d7bb532b15688fb4b3f5dcc70f
Charles Lee <charleslee90@gmail.com>
2015-08-25 21:21:05 +0800

    Implemented google maps api, commented out angular google maps

--- public/js/app.js
+++ public/js/app.js
@@ -3,8 +3,7 @@
 	var app = angular.module('hiddenFormosa', [
 		'ui.router',
 		'zumba.angular-waypoints', 
-		'duScroll',
-		'uiGmapgoogle-maps'
+		'duScroll'
 	]).value('duScrollOffset', 1000);
 
 	app.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function($stateProvider, $urlRouterProvider, $locationProvider) {
@@ -50,10 +49,129 @@
 
 	app.controller('mapController', ['$scope', function($scope) {
 
-		$scope.map = { 
-			center: { latitude: 23.75, longitude: 121 }, 
-			zoom: 8 
-		};
+		$scope.cities = [
+			{
+				'place' : 'Taipei',
+				'details' : 'Capital of Taiwan',
+				'lat' : '25.0333',
+				'long' : '121.6333'
+			},
+			{
+				'place' : 'Keelung',
+				'details' : 'Heart of new Taipei',
+				'lat' : '25.1333',
+				'long' : '121.7333'
+			},
+			{
+				'place' : 'Ping Tung',
+				'details' : 'Aborigine culture',
+				'lat' : '22.6755',
+				'long' : '120.4914'
+			},
+			{
+				'place' : 'Kaohsiung',
+				'details' : 'Southern most City of Taiwan',
+				'lat' : '22.6333',
+				'long' : '120.2667'
+			},
+			{
+				'place' : 'Taichung',
+				'details' : 'Center City of Taiwan',
+				'lat' : '24.1500',
+				'long' : '120.6667'
+			},
+		];
+
+		var mapOptions = {
+			zoom: 8,
+			center: new google.maps.LatLng(23.75,121),
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		}
+
+		$scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+		$scope.markers = [];
+
+		var infoWindow = new google.maps.InfoWindow();
+
+		var createMarker = function (info) {
+
+			var marker = new google.maps.Marker({
+				map: $scope.map,
+				position: new google.maps.LatLng(info.lat, info.long),
+				title: info.place
+			});
+
+			marker.content = '<div class="infoWindowContent"><p class="marker-details">' + info.details + '</p></div>';
+
+			google.maps.event.addListener(marker, 'click', function(){
+				infoWindow.setContent('<h4><a class="marker-link" href="#">' + marker.title + '</a></h4>' + marker.content);
+				infoWindow.open($scope.map, marker);
+			});
+
+			$scope.markers.push(marker);
+		}
+
+		for (i = 0; i < $scope.cities.length; i++){
+			createMarker($scope.cities[i]);
+		}
+
+		// $scope.openInfoWindow = function(e, selectedMarker){
+		// 	e.preventDefault();
+		// 	google.maps.event.trigger(selectedMarker, 'click');
+		// }
+
+
+
+
+		// // ANGULAR MAPS 
+		// // =========================================
+		// $scope.markers = [
+		// 	{
+		// 		"id": "1",
+		// 		"latitude": "23.5",
+		// 		"longitude": "121",
+		// 		"title": "Location 1"
+		// 	}, 
+		// 	{
+		// 		"id": "2",
+		// 		"latitude": "24.00",
+		// 		"longitude": "121",
+		// 		"title": "Location 2"
+		// 	}, 
+		// 	{
+		// 		"id": "3",
+		// 		"latitude": "24.5",
+		// 		"longitude": "121",
+		// 		"title": "Location 3"
+		// 	}
+		// ];
+
+		// $scope.window = {
+		// 	model: {},
+		// 	show: false,
+		// 	options: {
+	 //          pixelOffset: {width:-1,height:-30}
+	 //        },
+		// 	closeClick: function() {
+  //               this.show = false;
+  //           }
+		// }
+
+		// $scope.map = {
+		// 	center: { latitude: 23.75, longitude: 121 },
+		// 	control: {},
+		// 	zoom: 8,
+		// 	window: $scope.window,
+		// 	markers: $scope.markers,
+		// 	markersEvents: {
+		// 		click: function(marker, eventName, model, args) {
+		// 			$scope.map.window.model = model;
+		// 			$scope.map.window.show = true;
+			  
+		// 		}
+		// 	}
+		// };
 
 	}])
 
